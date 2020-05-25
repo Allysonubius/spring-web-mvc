4223d0c4d6e79b60d5cee2ba6ab1653977931f60
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-05-25 14:07:06 -0300

    Finalizado video aula sobre Entendendo o Spring Web MVC na linguagem Java sobre Frameworks.

---
+++ Spring-Web-MVC/src/main/java/com/DIO/SpringWebMVC/Repository/JediRepository.java
@@ -0,0 +1,25 @@
+package com.DIO.SpringWebMVC.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.DIO.SpringWebMVC.model.Jedi;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JediRepository {
+    private List<Jedi> jedi;
+
+    public JediRepository(List<Jedi> jedi) {
+        jedi = new ArrayList<> ();
+
+        jedi.add(new Jedi("Luke", "Skylwalker"));
+    }
+
+    public List<Jedi> getAllJEdi(){
+
+        return this.jedi;
+    }
+    public void add(final Jedi jedi){
+        this.jedi.add(jedi);
+    }
+}
---
+++ Spring-Web-MVC/src/main/java/com/DIO/SpringWebMVC/controller/jediController.java
@@ -0,0 +1,49 @@
+package com.DIO.SpringWebMVC.controller;
+
+import com.DIO.SpringWebMVC.Repository.JediRepository;
+import com.DIO.SpringWebMVC.model.Jedi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class jediController {
+
+    @Autowired
+    private JediRepository repository;
+
+    @GetMapping ("/jedi")
+    public ModelAndView jedi(){
+
+        final ModelAndView modelAndView = new ModelAndView ();
+        modelAndView.setViewName ("Jedi");
+
+        modelAndView.addObject ("allJedi",repository.getAllJEdi ());
+
+        return modelAndView;
+    }
+    @GetMapping("/new-jedi")
+    public ModelAndView newJedi(){
+
+        final ModelAndView modelAndView = new ModelAndView ();
+        modelAndView.setViewName ("new-jedi");
+
+        modelAndView.addObject ("jedi", new Jedi());
+
+        return modelAndView;
+    }
+    @PostMapping("/jedi")
+    public String createJedi(@Validated @ModelAttribute Jedi jedi){
+        repository.add(jedi);
+
+        return "redirect:jedi";
+
+    }
+
+}
