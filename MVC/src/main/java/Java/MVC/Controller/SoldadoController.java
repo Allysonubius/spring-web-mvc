c063f3346cd2b47639ec08b847445ec33b18b6d3
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-05-26 21:00:51 -0300

    Iniciado Aplicações com Spring Web MVC - nivel Intermediário.

---
+++ MVC/src/main/java/Java/MVC/Controller/SoldadoController.java
@@ -0,0 +1,9 @@
+package Java.MVC.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/soldado")
+public class SoldadoController {
+}
